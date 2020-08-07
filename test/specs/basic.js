6c26aa99d1515fe80e46e5c9deb51947b8d06a3e
Arnav Kohli <arnavkohli@gmail.com>
2020-08-07 10:17:27 +0530

    Final push

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -6,12 +6,11 @@ describe('broken link test', function() {
     it('should check the page for broken image links', async () => {
         
         // Waiting for browser to load page
-        await browser.url("https://www.americanexpress.com/in/")
+        await browser.url("https://www.americanexpress.com/us")
         browser.debug();
 
         // Exracting all <img> elements
         const links = await $$('<img>');
-
         // Extracting src of all <img> elements
         const urls = links.map(link => link.getAttribute('src'));
         var filtered_urls;
@@ -20,8 +19,11 @@ describe('broken link test', function() {
         await Promise.all(urls).then(response => {filtered_urls = response})
 
         // Filtering them and removing the garbage ones
-        filtered_urls = filtered_urls.filter(u => u.startsWith("https"))
-        console.log('URLS', filtered_urls);
+        filtered_urls = filtered_urls.filter(function(u) {
+            if (u === null)
+                return false;
+            return u.startsWith("https");
+        });
 
         // Sending a request to all URLs
         const requests = filtered_urls.map(url => axios.get(url));
@@ -32,9 +34,15 @@ describe('broken link test', function() {
         // Checking status codes for all responses
         const statusCodes = responses.map(response => response.status);
 
+        var errored = [];
         // All status codes should be 200
-        statusCodes.forEach(statusCode => {
-            expect(statusCode).to.be.equal(200);
-        })
+        for (var i = 0; i < statusCodes.length; i++){
+            console.log(statusCodes[i], filtered_urls[i])
+            if (statusCodes[i] !== 200)
+                errored.push(filtered_urls[i])
+            expect(statusCodes[i]).to.be.equal(200);
+        }
+
+        console.log('[ERRORED_OUT_URLS]', errored);
     });
 });
